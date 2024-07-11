645ce6e7bdd9e50c05302cdf88e8a3c4baff6eac
Christopher Mann <christopher.mann@dbosoft.eu>
2024-07-11 17:41:22 +0200

    Add boiler plate

---
+++ Setup-LocalGenePool.ps1
@@ -0,0 +1,24 @@
+#Requires -Version 7.4
+
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+. $PSScriptRoot/Use-Settings.ps1
+
+function Setup-Gene {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $GeneSetTag
+    )
+    eryph-packer geneset-tag pack $GeneSetTag --workdir (Join-Path $PSScriptRoot "genesets")
+    Remove-Item -Path (Join-Path $EryphSettings.LocalGenePoolPath $GeneSetTag) -Force -Recurse -ErrorAction SilentlyContinue
+    Copy-Item -Path (Join-Path $PSScriptRoot "genesets" $GeneSetTag ".packed") `
+        -Destination (Join-Path $EryphSettings.LocalGenePoolPath $GeneSetTag) `
+        -Recurse
+}
+
+foreach ($manifestPath in (Get-ChildItem -Path (Join-Path $PSScriptRoot "genesets") -Filter "geneset-tag.json" -Recurse)) {
+    $manifest = Get-Content -Raw -Path $manifestPath | ConvertFrom-Json -AsHashtable
+    Setup-Gene -GeneSetTag $manifest.geneset
+}
