46808a3cc2760aa103a7efdf12ca0bce9a43fe8b
Christopher Mann <christopher.mann@dbosoft.eu>
2024-09-03 16:19:24 +0200

    Improve catlet validation (#4)
    * Validate the cloud-init status of the catlet under test
    * Introduce a proper retry mechanism for connecting to the catlet under test
    * Support environment variables for configuring certain test paths

--- tests/Helpers.ps1
+++ tests/Helpers.ps1
@@ -13,17 +13,59 @@ function Connect-Catlet {
 
     [Parameter()]
     [securestring]
-    $Password = (ConvertTo-SecureString "e2e" -AsPlainText -Force)
+    $Password = (ConvertTo-SecureString "e2e" -AsPlainText -Force),
+
+    [Parameter()]
+    [switch]
+    $WaitForCloudInit
   )
   
   $null = Start-Catlet -Id $CatletId -Force
   $catletIp = Get-CatletIp -Id $CatletId
-    
-  Start-Sleep -Seconds 5
-    
+  
+  # Remove the existing known host entry to avoid host key verification
+  # errors. We expect the host key to change as we just created a new catlet.
+  $null = Remove-SSHTrustedHost -HostName $catletIp.IpAddress
+
+  $timeout = New-TimeSpan -Minutes 10
+  $start = Get-Date
   $credentials = [PSCredential]::New($Username, $Password)
-    
-  return New-SSHSession -ComputerName $catletIp.IpAddress -Credential $credentials -AcceptKey -Force
+  $sshSession = $null
+
+  # Retry until the SSH session is established or the timeout is reached.
+  # Depending on the state of the catlet and the network, a connection
+  # attempt can either timeout or immediately fail.
+  while (-not $sshSession) {
+    try {
+      $sshSession = New-SSHSession -ComputerName $catletIp.IpAddress -Credential $credentials -AcceptKey
+    } catch {
+      if (((Get-Date) - $start) -gt $timeout) {
+        throw
+      }
+      Start-Sleep -Seconds 5
+    }
+  }
+
+  if (-not $WaitForCloudInit) {
+    return $sshSession
+  }
+  
+  # Wait for cloud-init to finish. This is necessary as the SSH connection might already
+  # succeed while cloud-init is still running. Additionally, this also ensures that
+  # cloud-init did not fail.
+  while ($true) {
+    $result = Invoke-SSHCommand -Command "cloud-init status" -SSHSession $sshSession
+    if (($result.Output -inotlike '*not started*') -and ($result.Output -inotlike '*running*')) {
+      if (($result.Output -ilike '*degraded*') -or ($result.Output -ilike '*error*')) {
+        throw "cloud-init reported an error: $($result.Output)"
+      }
+      return $sshSession
+    }
+    if (((Get-Date) - $start) -gt $timeout) {
+      throw 'cloud-init did not finish within the timeout'
+    }
+    Start-Sleep -Seconds 5
+  }
 }
 
 function New-TestProject {
