cb69c0f2ed9cb5d05e3db813f99d8c3e327aaab5
Christopher Mann <christopher.mann@dbosoft.eu>
2024-09-03 16:47:55 +0200

    * Add CI script
    * Add catlet config for the runner

---
+++ Run-Tests.ps1
@@ -0,0 +1,32 @@
+#Requires -Version 7.4
+
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+Write-Output "Going to run eryph end-to-end tests"
+
+Write-Output "Installing required Powershell modules..."
+Install-Module -Name Pester -Force -Scope CurrentUser
+Install-Module -Name Assert -Force -Scope CurrentUser
+Install-Module -Name Posh-SSH -Force -Scope CurrentUser
+Install-Module -Name Eryph.ComputeClient -Force -Scope CurrentUser
+
+. $PSScriptRoot/Use-Settings.ps1
+
+Write-Output "The following settings are configured"
+Write-Output "EryphZeroPath: $($EryphSettings.EryphZeroPath)"
+Write-Output "EryphPackerPath: $($EryphSettings.EryphPackerPath)"
+Write-Output "ComputeClientPath: $($EryphSettings.ComputeClientPath)"
+Write-Output "LocalGenePoolPath: $($EryphSettings.LocalGenePoolPath)"
+Write-Output "Path: $($Env:Path)"
+
+Write-Output "Setting up local gene pool"
+& $PSScriptRoot/Setup-LocalGenePool.ps1
+
+Write-Output "Running tests..."
+$pesterConfig = New-PesterConfiguration
+$pesterConfig.Output.Verbosity = 'Detailed'
+$pesterConfig.Run.Exit = $True
+$pesterConfig.Run.Path = "$PSScriptRoot/tests"
+$pesterConfig.TestResult.Enabled = $True
+Invoke-Pester -Configuration $pesterConfig
