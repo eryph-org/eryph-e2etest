514a0cdae0e1cef1251d79c28a4b487c533d7f52
Christopher Mann <christopher.mann@dbosoft.eu>
2025-01-15 11:08:30 +0100

    Cleanup

--- tests/Catlets.Tests.ps1
+++ tests/Catlets.Tests.ps1
@@ -16,7 +16,7 @@ Describe "Catlets" {
   
   Context "New-Catlet" {
 
-    It "Creates catlet without config" -Tag Smoke {
+    It "Creates catlet without config" {
       $config = @'
 name: catlet
 '@
