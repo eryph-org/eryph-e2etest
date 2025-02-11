1464f6bebc1ae911dd085c89d460ed29556d1823
Christopher Mann <christopher.mann@dbosoft.eu>
2025-02-11 16:19:42 +0100

    Add tests

--- tests/Catlets.Tests.ps1
+++ tests/Catlets.Tests.ps1
@@ -502,6 +502,147 @@ capabilities:
     }
   }
 
+  Context "Test-CatletConfig" {
+    It "Quickly validates catlet configuration and returns errors" {
+      $config = @'
+name: invalid$name
+'@
+      $result = Test-CatletConfig -Config $config -Quick
+
+      $result.IsValid | Should -BeFalse
+      $result.Errors | Should -HaveCount 1
+      $result.Errors[0].Message | Should -BeLike "The catlet name contains invalid characters.*"
+    }
+
+    It "Expands configuration for new catlet" {
+      $config = @"
+name: $catletName
+variables:
+- name: userName
+  required: true
+fodder:
+- name: add-user-greeting
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo 'Hello {{ userName }}!' >> hello-world.txt
+- name: write-vm-id
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo '{{ vmId }}' >> hyperv-vm-id.txt
+"@
+
+      $result = Test-CatletConfig -Config $config -Variables @{ userName = "Eve" } -SkipVariablesPrompt
+
+      $result | Should -Be @"
+name: $catletName
+cpu:
+  count: 1
+memory:
+  startup: 1024
+networks:
+- name: default
+  adapter_name: eth0
+variables:
+- name: userName
+  value: Eve
+  required: true
+- name: catletId
+  type: String
+  value: <<catletId>>
+  secret: false
+  required: false
+- name: vmId
+  type: String
+  value: <<vmId>>
+  secret: false
+  required: false
+fodder:
+- name: add-user-greeting
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo 'Hello Eve!' >> hello-world.txt
+  secret: false
+- name: write-vm-id
+  type: shellscript
+  content: |-
+    #!/bin/bash
+    echo '<<vmId>>' >> hyperv-vm-id.txt
+  secret: false
+
+"@
+
+  }
+
+    It "Expands configuration for existing catlet" {
+      $config = @"
+name: $catletName
+project: $($project.Name)
+variables:
+- name: userName
+  required: true
+fodder:
+- name: add-user-greeting
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo 'Hello {{ userName }}!' >> hello-world.txt
+- name: write-vm-id
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo '{{ vmId }}' >> hyperv-vm-id.txt
+"@
+
+      $catlet = New-Catlet -Config $config -Variables @{ userName = "Eve" } -SkipVariablesPrompt
+      $vm =  Get-VM $catletName
+
+      $result = Test-CatletConfig -Config $config -Id $catlet.Id
+
+      $result | Should -Be @"
+project: $($project.Name)
+name: $catletName
+cpu:
+  count: 1
+memory:
+  startup: 1024
+networks:
+- name: default
+  adapter_name: eth0
+variables:
+- name: userName
+  value: Eve
+  required: true
+- name: catletId
+  type: String
+  value: $($catlet.Id)
+  secret: false
+  required: false
+- name: vmId
+  type: String
+  value: $($vm.Id)
+  secret: false
+  required: false
+fodder:
+- name: add-user-greeting
+  type: shellscript
+  content: |
+    #!/bin/bash
+    echo 'Hello Eve!' >> hello-world.txt
+  secret: false
+- name: write-vm-id
+  type: shellscript
+  content: |-
+    #!/bin/bash
+    echo '$($vm.Id)' >> hyperv-vm-id.txt
+  secret: false
+s
+"@
+    }
+  }
+
   Context "Inventory" {
     It "Updates inventory when catlet is changed in Hyper-V" {
       $config = @"
