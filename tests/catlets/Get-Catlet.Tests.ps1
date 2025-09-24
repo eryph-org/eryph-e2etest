6484cd72419cb8be3731efab554a8d09f5210943
Christopher Mann <christopher.mann@dbosoft.eu>
2025-09-24 17:21:30 +0200

    Split catlet preparation and host deployment  (#39)
    * Align existing tests with new update logic
    * Add inventory tests

--- tests/catlets/Get-Catlet.Tests.ps1
+++ tests/catlets/Get-Catlet.Tests.ps1
@@ -15,6 +15,40 @@ Describe "Catlets" {
   }
 
   Context "Get-Catlet" {
+
+    It "Returns the catlet configuration" {
+      $config = @"
+name: $catletName
+"@
+
+      $catlet = New-Catlet -Name $catletName -ProjectName $project.Name -Config $config
+
+      $expectedConfig = @"
+config_type: instance
+project: $($project.Name)
+name: $catletName
+location: *
+hostname: $catletName
+environment: default
+store: default
+cpu:
+  count: 1
+memory:
+  startup: 1024
+network_adapters:
+- name: eth0
+  mac_address: d2:ab:*
+networks:
+- name: default
+  adapter_name: eth0
+  subnet_v4:
+    name: default
+    ip_pool: default
+
+"@
+      $catletConfig = Get-Catlet -Id $catlet.Id -Config
+      $catletConfig | Should -BeLikeExactly $expectedConfig
+    }
     
     It "Returns the catlet configuration with capabilities" {
       $config = @'
@@ -30,23 +64,35 @@ capabilities:
       $catlet = New-Catlet -Name $catletName -ProjectName $project.Name -Config $config
 
       $expectedConfig = @"
-version: 1.0
+config_type: instance
 project: $($project.Name)
 name: $catletName
 location: *
+hostname: $catletName
+environment: default
+store: default
 cpu:
   count: 1
 memory:
   startup: 1024
   minimum: 512
   maximum: 1048576
+network_adapters:
+- name: eth0
+  mac_address: d2:ab:*
 capabilities:
+- name: dynamic_memory
 - name: nested_virtualization
 - name: secure_boot
   details:
   - template:MicrosoftUEFICertificateAuthority
-- name: dynamic_memory
 - name: tpm
+networks:
+- name: default
+  adapter_name: eth0
+  subnet_v4:
+    name: default
+    ip_pool: default
 
 "@
       $catletConfig = Get-Catlet -Id $catlet.Id -Config
